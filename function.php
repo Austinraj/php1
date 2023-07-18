077260963783f7423fd1fd97ef0f34aa3255f536
Austinraj <139209807+Austinraj@users.noreply.github.com>
2023-07-18 18:49:03 +0530

    Add files via upload

---
+++ function.php
@@ -0,0 +1,20 @@
+<?php
+
+function student($name){
+
+	echo "name: $name";
+	echo "<br>";
+}
+
+student("austin");
+
+
+function onlineshopping($myorder){
+
+	echo "food:$myorder";
+	echo "<br>";
+}
+
+onlineshopping("chicken biryani");
+
+?>
